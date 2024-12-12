f283b092bc56a54d06ef077223537f20b5f18bf2
Hiba-Bibi <155488914+Hiba-Bibi@users.noreply.github.com>
2024-12-12 17:48:10 +0100

    Update PredictionServlet.java

--- p02-cloud/src/main/java/org/mql/cloud/web/PredictionServlet.java
+++ p02-cloud/src/main/java/org/mql/cloud/web/PredictionServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 import weka.classifiers.trees.J48;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
